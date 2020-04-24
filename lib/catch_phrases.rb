676cd087bc946a560cfd287966d8edf8a5834041
Andrew Hawley <ahawley785@gmail.com>
2020-04-24 20:48:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  
+
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -8,5 +8,3 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-
-
